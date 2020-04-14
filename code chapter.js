65a5a57b33553d21458514724db09d2a1a76b06e
longkingstar <46622144+longkingstar@users.noreply.github.com>
2020-04-14 12:48:32 +0700

    Add files via upload

---
+++ code chapter.js
@@ -0,0 +1,36 @@
+<!----------Đầu---------------->
+<div style="clear: both; text-align: center;">
+<img src="https://3.bp.blogspot.com/--ey6hRtag3g/XUawFPAEfPI/AAAAAAAABMo/9_cEuKGdigU7sSjp3_UovZ43WY7Ms7IjwCLcBGAs/s1600/0.png" />
+<!-- ---------------------------------------------------------------------------------------------------------------------------- -->
+
+<img src="#" />
+
+<img src="https://3.bp.blogspot.com/-j2oQmc-uZ0I/XUawFJ1PdAI/AAAAAAAABMk/9TymYJTNBtEZ83VXLLqAE4dXcbs_jc5MACLcBGAs/s1600/000.png" />
+
+<img src="https://3.bp.blogspot.com/-eU9GUHdd1aU/XUawF2W8VJI/AAAAAAAABMs/HRCwKXD0xWECAxLXtqidNxmKDCFx_TBvgCLcBGAs/s1600/0000.png" />
+
+
+</div>
+<style>
+img {vertical-align: middle;}
+</style>
+
+<div id="cuon">
+<a href="#" id="cuon-active"><i class="fas fa-angle-double-left"></i></a>
+
+<div id="cuon-centered">
+<a><select id="dorong" onchange="location = this.value;" style="font-size: 20px;padding: 5px 0px;border-radius: 5px;">&gt;
+ <option value="#">Chapter 1</option>
+   <option value="#">Chapter 2</option>
+   <option value="#">Chapter 3</option>
+   <option selected="" value="#">Chapter 4</option>
+
+   
+</select>
+</a>
+</div>
+<div id="cuon-right">
+<a href="#" id="cuon-active"><i class="fas fa-angle-double-right"></i></a>
+</div>
+</div>
+<!----------Cuối---------------->
